a53b875652ca5670a62b87aa9378181fc7837648
Sagar Bhatt <sagarbhatt7451@gmail.com>
2025-03-25 09:18:07 +0530

    send code from home

--- src/app/_components/FooditemList.js
+++ src/app/_components/FooditemList.js
@@ -1,4 +1,28 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const FooditemList = () => {
+let [fooditems, setFooditems] = useState();
+  useEffect(()=>{
+LoodFoodItems();
+  },[]);
+
+  const LoodFoodItems = async ()=>{
+    let response = await fetch("http://localhost:3000/api/foods/67d850bcf463ee431fb09bc3");
+    response = await response.json();
+    console.log(response);
+    if(response.success){
+    setFooditems(response.result);
+   
+      
+
+    }
+    else{
+      alert("item not loading");
+    }
+  };
+  console.log(typeof(fooditems));
+  console.log(JSON.stringify(fooditems, null, 3));
   return (
     <div>
       <div>
@@ -16,18 +40,31 @@ const FooditemList = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Pizza</td>
-              <td>120</td>
-              <td>http://localhost:3000/restaurant/dashboard</td>
-              <td>I LOVE PIZZA</td>
-              <td>---------</td>
-          <td>
-                    <button className="btn btn-primary">Edit</button>                                                              
-                    <button className="btn btn-danger ms-2">Delete</button>                                                              
-              </td>
-            </tr>
+            
+            {
+
+for(let x in fooditems){
+
+<tr>
+<th scope="row">1</th>
+<td>Pizza</td>
+<td>120</td>
+<td>http://localhost:3000/restaurant/dashboard</td>
+<td>I LOVE PIZZA</td>
+<td>---------</td>
+<td>
+      <button className="btn btn-primary">Edit</button>                                                              
+      <button className="btn btn-danger ms-2">Delete</button>                                                              
+</td>
+</tr>
+}
+            }
+
+             
+                
+              
+    
+           
           </tbody>
         </table>
       </div>
